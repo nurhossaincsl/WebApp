0d68e0dd6da2963af50f71c165f19430d76ece6b
nurhossaincsl <nur.cse.bd@gmail.com>
2026-08-20 21:52:44 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<h1>Welcome to Web Page</h1>
+<br>
+<h2>CI/CD</h2> <br>
+<?php
+date_default_timezone_set('Asia/Dhaka');
+
+echo "Current Date: " . date("Y-m-d") . "<br>";
+echo "Current Time: " . date("H:i:s") . "<br>";
+echo "Current Date & Time: " . date("Y-m-d H:i:s");
+?>
