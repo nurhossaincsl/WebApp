febe850c412697ef92b63ba17471acbd17909bb3
nurhossaincsl <nur.cse.bd@gmail.com>
2026-08-20 21:56:26 +0600

    Update index.php
    Update project status

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <h2>CI/CD</h2> <br>
 <?php
 date_default_timezone_set('Asia/Dhaka');
-
-echo "Current Date: " . date("Y-m-d") . "<br>";
-echo "Current Time: " . date("H:i:s") . "<br>";
 echo "Current Date & Time: " . date("Y-m-d H:i:s");
 ?>
+<br>
+<center></center><b>Project Under construction</b>b></center>
